e671148b28df7a5407b9bb7971554072d06ce41e
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-24 20:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your code here
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+
+const images = document.querySelectorAll(".image");
+
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
+} 
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+@@ -43,15 +43,4 @@
+  image.ondrop = drop;
+};
+
+images.forEach(dragdrop);
+	div.addEventListener('drop',(e)=>{
+		const drag = document.getElementById(e.dataTransfer.getData('id'))
+	    let tid = drag.id
+		drag.id = div.id
+		div.id = tid
+		let temp = div.innerText
+		div.innerText  = drag.innerText
+		drag.innerText = temp
+		console.log(div.innerText, drag.innerText, temp)
+	})
+})
+images.forEach(dragdrop);
