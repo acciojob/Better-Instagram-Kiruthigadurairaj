f3b46e7db980728c4d32ef1993177a7458c6e0fb
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-25 12:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-let dragindex = 0;
-let dropindex = 0;
-let clone = "";
+document.addEventListener('DOMContentLoaded', () => {
+  const images = document.querySelectorAll('.image');
+  let dragSrcEl = null;
 
-const images = document.querySelectorAll(".image");
+  function handleDragStart(e) {
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.outerHTML);
+    this.classList.add('dragging');
+  }
 
-function drag(e) {
-  e.dataTransfer.setData("text", e.target.id);
-} 
-}
+  function handleDragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  }
 
-function allowDrop(e) {
-  e.preventDefault();
-@@ -43,15 +43,4 @@
-  image.ondrop = drop;
-};
+  function handleDragEnter() {
+    this.classList.add('over');
+  }
 
-images.forEach(dragdrop);
-	div.addEventListener('drop',(e)=>{
-		const drag = document.getElementById(e.dataTransfer.getData('id'))
-	    let tid = drag.id
-		drag.id = div.id
-		div.id = tid
-		let temp = div.innerText
-		div.innerText  = drag.innerText
-		drag.innerText = temp
-		console.log(div.innerText, drag.innerText, temp)
-	})
-})
-images.forEach(dragdrop);
+  function handleDragLeave() {
+    this.classList.remove('over');
+  }
+
+  function handleDrop(e) {
+    e.stopPropagation();
+    if (dragSrcEl !== this) {
+      dragSrcEl.parentNode.removeChild(dragSrcEl);
+      const dropHTML = e.dataTransfer.getData('text/html');
+      this.insertAdjacentHTML('beforebegin', dropHTML);
+      const droppedElement = this.previousSibling;
+      addDragAndDropListeners(droppedElement);
+    }
+    this.classList.remove('over');
+  }
+
+  function handleDragEnd() {
+    images.forEach((image) => {
+      image.classList.remove('over');
+      image.classList.remove('dragging');
+    });
+  }
+
+  function addDragAndDropListeners(element) {
+    element.addEventListener('dragstart', handleDragStart, false);
+    element.addEventListener('dragenter', handleDragEnter, false);
+    element.addEventListener('dragover', handleDragOver, false);
+    element.addEventListener('dragleave', handleDragLeave, false);
+    element.addEventListener('drop', handleDrop, false);
+    element.addEventListener('dragend', handleDragEnd, false);
+  }
+
+  images.forEach((image) => addDragAndDropListeners(image));
+});
